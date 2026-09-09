aa8ad2bbbfe30e9d69bef5b473e50757e1de28a8
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-09 02:36:00 +0100

    Fix formatting issues in run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+sudo /Applications/Docker.app/Contents/MacOS/install --user testuser --proxy-http-mode="manual" --override-proxy-pac="http://localhost:8080/myproxy.pac"
+sudo hdiutil attach Docker.dmg
+sudo /Volumes/Docker/Docker.app/Contents/MacOS/install
+sudo hdiutil detach /Volumes/Docker
